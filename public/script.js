cb8f9fdf3d3d32c97b66f0cab1f286c2e4a73ae2
Zhaoyuan Sun <sunz1@rose-hulman.edu>
2019-01-29 23:49:23 -0500

    update

--- public/script.js
+++ public/script.js
@@ -1,13 +1,29 @@
 window.onload = function () {
-    var converter = new showdown.Converter();
+    var converter = new showdown.Converter({'tables': true});
     var pad = document.getElementById('pad');
     var markdownArea = document.getElementById('markdown');
+
+    // set options for converter
+    // showdown.setOption('tables', true);
+    // converter.setOption('parseImgDimensions', true);
+    // converter.setOption('ghCompatibleHeaderId', true);
+    // showdown.setOption('tasklists', true);
+    // converter.setOption('simpleLineBreaks', true);
+    // converter.setOption('openLinksInNewWindow', true);
+    // converter.setOption('emoji', true);
+
+    // var text = "- [ ] task1";
+    // console.log(converter.makeHtml(text));
+    console.log(converter.getOptions());
 
     var previousMarkdownValue;
 
     var convertTextAreaToMarkdown = function () {
         var markdownText = pad.value;
+        console.log(pad.innerHTML);
+        // console.log(converter.getOptions());
         html = converter.makeHtml(markdownText);
+        console.log(html);
         markdownArea.innerHTML = html;
     };
 
